568a0215a79b7977d2e40f42c2a58352cc8579f7
CGReinhold <cleyson.reinhold@gmail.com>
2018-10-23 20:15:48 -0300

    Adicionado menu e separado jogo para mais fases

---
+++ game/boot.js
@@ -0,0 +1,53 @@
+var bootState = {
+  // Within every state callback, `this` is the current state and `this.game` is the game.
+
+  init: function init() {
+    // Phaser.Canvas.setImageRenderingCrisp(this.game.canvas);
+    // this.game.renderer.renderSession.roundPixels = true;
+  },
+
+  // The loader starts automatically after `preload`.
+
+  preload: function preload() {
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('wall', 'assets/wall.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('halfGround', 'assets/halfPlatform.png');
+    this.load.image('bigGround', 'assets/bigPlatform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('door', 'assets/door.png');
+    this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+    this.load.spritesheet('mainWalk', 'assets/mainWalk.png', { frameWidth: 42, frameHeight: 48 });
+    this.load.spritesheet('mainWait', 'assets/mainWait.png', { frameWidth: 42, frameHeight: 48 });
+    this.load.spritesheet('mainDie', 'assets/mainDie.png', { frameWidth: 60, frameHeight: 48 });
+    this.load.spritesheet('enemyWait', 'assets/enemyWait.png', { frameWidth: 33, frameHeight: 48 });
+  },
+
+  // `loadUpdate` runs while the loader is loading.
+  // `setPreloadSprite` updates the loading bar automatically, so we don't need to do anything else.
+  // We'll just print the loader progress.
+
+  loadUpdate: function loadUpdate() {
+    console.info('load.hasLoaded', this.load.hasLoaded);
+    console.info('load.progress', this.load.progress);
+  },
+
+  // `loadRender` runs while the loader is loading.
+
+  loadRender: function loadRender() {
+    this.game.debug.text('Loading: ' + this.load.progress, 20, 20);
+  },
+
+  // `create` runs once the loader has finished.
+  // All we need to do is start the next state.
+
+  create: function create() {
+    this.scene.start('menu');
+  },
+
+  // Our handler for the `game.state.onStateChange` signal:
+
+  onStateChange: function onStateChange(newState, oldState) {
+    console.info('Changed state from "%s" to "%s"', oldState || '[none]', newState);
+  }
+};
---
+++ game/menu.js
@@ -0,0 +1,7 @@
+var menuState = {
+  create: function create() {
+    this.title = this.add.text(450, 200, 'Skeleton War', { fill: 'white', font: '96px serif', strokeThickness: 3 });
+    this.subtitle = this.add.text(650, 320, 'Iniciar', { fill: 'yellow', font: '32px sans-serif' });
+    this.input.on('pointerdown', function () { this.scene.start('game'); }, this);
+  }
+};
