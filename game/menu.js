ffeef426b7f76ae29d32ce905021031e864a88c4
CGReinhold <cleyson.reinhold@gmail.com>
2018-11-28 20:38:56 -0200

    Adicionado vídeo final

--- game/menu.js
+++ game/menu.js
@@ -2,6 +2,6 @@ var menuState = {
   create: function create() {
     this.title = this.add.text(450, 200, 'Skeleton War', { fill: 'white', font: '96px serif', strokeThickness: 3 });
     this.subtitle = this.add.text(650, 320, 'Iniciar', { fill: 'yellow', font: '32px sans-serif' });
-    this.input.on('pointerdown', function () { this.scene.start('level1'); }, this);
+    this.input.on('pointerdown', function () { this.scene.start('level3'); }, this);
   }
 };
